21d9699cf25bfcded54ab1c72613a4594fd1e65a
Md Shahid <36432967+dshahid380@users.noreply.github.com>
2018-04-18 00:40:48 +0530

    Create header.php

---
+++ Inc/header.php
@@ -0,0 +1,19 @@
+<div class="container-fluid navigation">
+        <div class="row">
+            <div class="col-sm-12">
+                <nav class="navbar navbar-inverse navbar-fixed-top">
+                    <div class="col-sm-5">
+                    <div class="navbar-header">
+                        <a style="color:white;font-size:25px;" class="navbar-brand" href="#">Personal Blog</a>
+                    </div></div>
+                    <ul class="nav nav-pills">
+                        <li><a href="INDEX.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+                        <li><a href=""><i class="fa fa-file-text" aria-hidden="true"></i> Pages</a></li>
+                        <li><a href="Admin/Catagories.php"><i class="fa fa-list-alt" aria-hidden="true"></i> Catagories</a></li>
+                        <li><a href="Contact-Us.php"><i class="fa fa-phone" aria-hidden="true"></i> Contact us</a></li>
+                    </ul>
+
+                </nav>
+            </div>
+        </div>
+    </div>
