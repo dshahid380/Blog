d424b820dbbcd42f580a4a922bb2d78b58343405
Md Shahid <36432967+dshahid380@users.noreply.github.com>
2018-04-18 00:36:10 +0530

    Add files via upload

---
+++ Admin/inc/sidebar.php
@@ -0,0 +1,15 @@
+<div id="dashboard" style="padding-top:10px;" class="container-fluid">
+             <div class="row">
+                 <div class="col-md-3">
+                        <div class="list-group">
+                                <a href="Index1.php" class="list-group-item list-group-item-action active">
+                                       <i class="fa fa-tachometer"></i> Dashboard</a>
+                                <a href="#" class="list-group-item list-group-item-action ">
+                                 <i class="fa fa-file-text-o"></i> All Post <span class="badge">(14)</span>
+                                </a>
+                                <a href="Catagories.php" class="list-group-item list-group-item-action"><i class="fa fa-folder-open"></i> Catagories <span class="badge"> (14)</span></a>
+                                <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-comment"></i> Comments <span class="badge">(14)</span></a>
+                                <a href="Users.php" class="list-group-item list-group-item-action"><i class="fa fa-users"></i> User <span class="badge">(14)</span></a>
+                                
+                          </div>
+                 </div>
