fb9834caf88fa34661d0d655d55fb38c805c52b0
Md Shahid <36432967+dshahid380@users.noreply.github.com>
2018-04-18 00:35:39 +0530

    Create header.php

---
+++ Admin/inc/header.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-fixed-top navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="#">Md Shahid</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item ">
+            <a style="color:white" class="nav-link" href="#"><i class="fa fa-plus-square"></i> Add Post </a>
+          </li>
+          <li class="nav-item">
+            <a style="color:white" class="nav-link" href="#"><i class="fa fa-user-plus"></i> Add User</a>
+          </li>
+          <li class="nav-item">
+            <a style="color:white" class="nav-link" href="#"><i class="fa fa-user"></i> Profile</a>
+          </li>
+          <li class="nav-item">
+            <a style="color:white" class="nav-link" href="#"><i class="fa fa-power-off"></i> Logout</a>
+          </li>
+        </ul>
+       
+      </div>
